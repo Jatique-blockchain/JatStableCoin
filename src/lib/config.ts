f967993934dbd93657e62829139763c31fa74c03
Ezejaemmanuel <ezejaemmanuella244@gmail.com>
2024-07-27 08:07:16 +0100

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,9 +1,7 @@
-/* This code snippet is setting up a configuration object for a wallet connection application using
-TypeScript. Here's a breakdown of what it's doing: */
 'use client';
 
 import { http, createStorage, cookieStorage } from 'wagmi';
-import { sepolia, bscTestnet, blastSepolia } from 'wagmi/chains';
+import { sepolia } from 'wagmi/chains';
 import { Chain, getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { defineChain } from 'viem/chains/utils';
 
@@ -11,24 +9,25 @@ const projectId = process.env.NEXT_PUBLIC_WALLETCONNECTION_PROJECTID;
 if (!projectId) {
    throw new Error('NEXT_PUBLIC_WALLETCONNECTION_PROJECTID is not set');
 }
+
 const localhost = /*#__PURE__*/ defineChain({
    id: 31337,
-
    name: 'Localhost',
    nativeCurrency: {
       decimals: 18,
       name: 'Ether',
       symbol: 'ETH',
-
    },
    rpcUrls: {
       default: { http: ['http://127.0.0.1:8545'] },
    },
-})
+});
 
-const supportedChains: Chain[] = [localhost, sepolia];
-// const supportedChains: Chain[] = [localhost];
+// Determine the environment
+const isDevelopment = process.env.NODE_ENV === 'development';
 
+// Define supported chains based on the environment
+const supportedChains: Chain[] = isDevelopment ? [localhost, sepolia] : [sepolia];
 
 export const config = getDefaultConfig({
    appName: "WalletConnection",
